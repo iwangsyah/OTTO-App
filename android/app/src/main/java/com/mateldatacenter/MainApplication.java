5abdb95e8ed194be976683f72d4a40a29dd69bfe
Wahyu Irwansyah <wahyuiwangsyah@izy.ai>
2019-05-14 13:19:49 +0700

    custom keyboard

--- android/app/src/main/java/com/mateldatacenter/MainApplication.java
+++ android/app/src/main/java/com/mateldatacenter/MainApplication.java
@@ -3,6 +3,7 @@ package com.mateldatacenterapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactlibrary.RNCustomKeyboardKitPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import codes.simen.IMEI.IMEI;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCustomKeyboardKitPackage(),
             new VectorIconsPackage(),
             new LinearGradientPackage(),
             new IMEI(),
